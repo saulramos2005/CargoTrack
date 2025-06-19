f3d3a89dd8854112a70b024386792dac769b2e3d
saulramos2005 <Sauldra79@gmail.com>
2025-06-18 20:33:42 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="../src/app/admin/page.tsx"
+          href="../admin/page.tsx"
           target="_blank"
           rel="noopener noreferrer"
         >
